6e66382419800150e4e6370892323979da37c97a
Gabriel Vlad <gabriel.vlad@outlook.dk>
2023-06-03 03:23:59 +0200

    redirect that fits gatsby better
    redirect that fits gatsby better

---
+++ src/pages/digital-resume-card-website.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Redirect } from '@reach/router';
+
+const DigitalResumeCardWebsite = () => {
+  return <Redirect to="https://www.gabrielvlad.com" noThrow />;
+};
+
+export default DigitalResumeCardWebsite;
